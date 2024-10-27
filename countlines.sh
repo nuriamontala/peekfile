e553c97525fe86c8a8f37ffb68f3627aeef33a7f
Nuria <nuria.montala01@estudiant.upf.edu>
2024-10-27 11:05:42 +0100

    version 2: any nomber of files as arguments

--- countlines.sh
+++ countlines.sh
@@ -1,11 +1,15 @@
  #!/bin/bash
        
-if [[ $(cat $1 | wc -l) -eq 0 ]]
- then
-   echo $1: The file has 0 lines
-elif [[ $(cat $1 | wc -l) -eq 1 ]]
- then
-   echo $1: The file has 1 line
-else
-   echo $1: The file has $(cat $1 | wc -l) lines
-fi
+for file in $@
+ do
+  line_count=$(cat $file | wc -l)
+  if [[ $line_count -eq 0 ]]
+   then
+     echo $file: The file has 0 lines
+  elif [[ $line_count  -eq 1 ]]
+   then
+     echo $file: The file has 1 line
+  else
+     echo $file: The file has $line_count lines
+  fi
+done
